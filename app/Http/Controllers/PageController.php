6038311c813b4e40899bc47aec34299e2cc25541
Kolade <corlardey@gmail.com>
2021-06-22 02:48:46 +0100

    Initial commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+class PageController extends Controller
+{
+    public function about()
+    {
+        return "About us Page";
+    }
+    public function contact()
+    {
+        return "Contact Page";
+    }
+    public function submitContact()
+    {
+        
+        return "Contact Page";
+    }
+}
