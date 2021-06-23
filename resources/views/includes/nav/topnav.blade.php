9d72ac90452789d0123bfba677c5d61703ce1a21
Kolade <corlardey@gmail.com>
2021-06-23 09:11:33 +0100

    migrations

---
+++ resources/views/includes/nav/topnav.blade.php
@@ -0,0 +1,19 @@
+<nav>
+        <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">Laravel Answers</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Recent</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Popular</a>
+        </li>
+        <li class="nav-item">
+            <a class="btn btn-primary" href="#" tabindex="-1">Ask a Question</a>
+        </li>
+        </ul>
+        </nav>
---
+++ resources/views/template.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1  maximum-scale=1.0, user-scalable=0">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            ul{
+                padding: .5em;
+            }
+            li a{
+                text-decoration:none;
+                color:gray
+            }
+            li a:hover{
+                background-color: #adb5bd;
+                color:white
+            }
+        </style>
+    </head>
+    <body>
+            @include('includes/nav/topnav')
+
+
+            @yield('content') 
+        
+    </body>
+</html>
